0f370cdb6a923236ad9d339f2086bb64e309a21f
igor_averin@mobile <awerig47@gmail.com>
2022-05-17 17:38:42 +0300

    added methods: option selection, show/hide dropdown, set dropdown position.  event handlers added.  styles added

--- custom-elems/js/index.js
+++ custom-elems/js/index.js
@@ -5,53 +5,137 @@ const selectEl = document.querySelector(`.js-select`);
 class CustomMultipleSelect {
   constructor(elem, options) {
     this._originSelect = elem;
-    this.values = Array.from(this._originSelect).map(option => {
-      const { textContent: text } = option;
-      return text;
+    this._originOptions = Array.from(this._originSelect);
+    
+    // создаём массив оригинальных текстовых значений элементов option
+    this.textValues = this._originOptions.map(option => {
+      const { textContent: text, selected } = option;
+      return { text, selected };
     });
-    this._fakeSelect = this.createFakeSelect();
-
+    
+    // создаём экземпляр кастомного селекта
+    this._coverSelect = this.createCoverSelect();
+    
+    // прячем оригинальный селект
     // this._originSelect.hidden = `true`;
-    this._originSelect.insertAdjacentElement(`afterend`, this._fakeSelect);
+    
+    // добавляем кастомный селект вместо оригинального
+    this._originSelect.insertAdjacentElement(`afterend`, this._coverSelect);
+    this.setOptgroupPosition = this.setOptgroupPosition.bind(this);
   }
-
-  createFakeSelect() {
-    const wrapper = document.createElement(`div`);
-    const field = document.createElement(`input`);
-    const list = document.createElement(`ul`);
-
+  
+  // метод создания кастомного селекта
+  createCoverSelect() {
+    this.wrapperEl = document.createElement(`div`);
+    this.fieldEl = document.createElement(`input`);
+    this.selectedOptListEl = document.createElement(`ul`);
+    this.optListEl = document.createElement(`ul`);
+    
+    this.wrapperEl.className = `cover-select`;
+    this.fieldEl.className = `cover-select__input`;
+    this.optListEl.className = `cover-select__optgroup`;
+    
+    // обработчик кликов для выбора вариантов селекта
     const onChangeOriginSelectItemClick = (ev) => {
-      if(!ev.target.classList.contains(`js-fake-option`)) return;
+      if(!ev.target.classList.contains(`js-cover-option`)) return;
 
       const index = ev.target.dataset.index;
-
-      this._originSelect[index].selected = !this._originSelect[index].selected;
+      this.toggleOption(index);
+      this.setOptgroupPosition();
     }
-
+    
+    // обработчик для сортировки списка вариантов селекта на основании введённого в поле поиска значения
     const onSortOptionsFieldKeyup = (ev) => {
       const fieldValue = ev.target.value.toLowerCase();
-      this._fakeOptions.forEach(option => {
-        option.textContent.toLowerCase().startsWith(fieldValue) ? option.classList.remove(`fake-option-hidden`) : option.classList.add(`fake-option-hidden`);
+      this._coverOptions.forEach(option => {
+        option.textContent.toLowerCase().startsWith(fieldValue) && option.dataset.selected ? option.classList.remove(`cover-select__option_hidden`) : option.classList.add(`cover-select__option_hidden`);
       });
+      
+      this.setOptgroupPosition();
     }
+    
+    // обработчики отображения/скрытия списка вариантов
+    const onShowOptgroupFieldFocus = () => {
+      this.showCoverOptgroup();
+    };
+    
+    const onHideOptgroupWindowClick = ev => {
+      if(!ev.target.closest(`.cover-select`)) this.hideCoverOptgroup();
+    };
 
-    list.addEventListener(`click`, onChangeOriginSelectItemClick);
-    field.addEventListener(`keyup`, onSortOptionsFieldKeyup);
-
-    this._fakeOptions = this.values.map((text, index) => {
+    this.optListEl.addEventListener(`click`, onChangeOriginSelectItemClick);
+    this.selectedOptListEl.addEventListener(`click`, onChangeOriginSelectItemClick);
+    this.fieldEl.addEventListener(`keyup`, onSortOptionsFieldKeyup);
+    this.fieldEl.addEventListener(`focus`, onShowOptgroupFieldFocus);
+    window.addEventListener(`click`, onHideOptgroupWindowClick);
+    
+    // создаём массив вариантов селекта
+    this._coverOptions = this.textValues.map(({ text, selected }, index) => {
       const li = document.createElement(`li`);
-      li.className = `js-fake-option`;
+      li.classList.add(`cover-select__option`, `js-cover-option`);
       li.dataset.index = index;
+      li.dataset.selected = selected;
       li.textContent = text;
       
       return li;
     });
+    
+    // добавляем варианты в список
+    this._coverOptions.forEach(item => this.optListEl.append(item));
 
-    this._fakeOptions.forEach(item => list.append(item));
+    this.wrapperEl.append(this.fieldEl, this.selectedOptListEl, this.optListEl);
 
-    wrapper.append(field, list);
-
-    return wrapper;
+    return this.wrapperEl;
+  }
+  
+  toggleOption(index) {
+    const originOption = this._originSelect[index];
+    const coverOption = this._coverOptions[index];
+    
+    originOption.selected = !originOption.selected;
+    coverOption.dataset.selected = originOption.selected;
+    this.setSelectedList(index, originOption.selected);
+  }
+  
+  // позиционируем список вариантов в зависимости от положения относительно окна браузера
+  setOptgroupPosition() {
+    const windowHeight = document.documentElement.clientHeight;
+    const coverSelectFullHeight = this.wrapperEl.offsetHeight + this.optListEl.offsetHeight;
+    const coverSelectTop = this.wrapperEl.getBoundingClientRect().top;
+    const coverSelectBottomPos = coverSelectTop + coverSelectFullHeight;
+    const heightDiff = windowHeight - coverSelectBottomPos;
+    
+    if(heightDiff < 0) {
+      this.optListEl.style.top = `-${this.optListEl.offsetHeight}px`;
+    } else this.optListEl.style.top = `${this.wrapperEl.offsetHeight}px`;
+  }
+  
+  showCoverOptgroup() {
+    this.setOptgroupPosition();
+    
+    // записываем обработчик в свойство объекта для дальнейшего удаления
+    this.onSetPositionOptgroupWindowScroll = () => this.setOptgroupPosition();
+    
+    window.addEventListener(`scroll`, this.onSetPositionOptgroupWindowScroll);
+    
+    this.optListEl.classList.add(`cover-select__optgroup_visible`);
+  }
+  
+  hideCoverOptgroup() {
+    window.removeEventListener(`scroll`, this.onSetPositionOptgroupWindowScroll);
+    this.optListEl.classList.remove(`cover-select__optgroup_visible`);
+  }
+  
+  setSelectedList(index, selected) {
+    index = Number(index);
+    if(selected) {
+      this.selectedOptListEl.append(this._coverOptions[index]);
+    } else if(index === 0) {
+      this.optListEl.insertAdjacentElement(`afterbegin`, this._coverOptions[index]);
+    } else {
+      console.log(index - 1);
+      this.optListEl.querySelector(`[data-index="${index - 1}"]`).insertAdjacentElement(`afterend`, this._coverOptions[index]);
+    }
   }
 }
 
