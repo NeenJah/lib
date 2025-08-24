f52ab07637c8d9efffa34c4cd1107a85563a3f5b
NeenjahPhone <awerig47@gmail.com>
2025-08-24 09:05:00 +0300

    дописан функционал линейного уравнения

--- linear-function/scripts/index.js
+++ linear-function/scripts/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
+const PATTERN_POINT = /^\d+\.?\d*,\d+\.?\d*$/;
 const linearForm = document.forms.linearForm;
 const btnSubmit = document.querySelector('#js-linear-form-submit');
 const point1 = document.forms.linearForm.point1;
 const point2 = document.forms.linearForm.point2;
 const xResult = document.forms.linearForm.xResult;
 const zResult = document.forms.linearForm.zResult;
-const patternPoint = /^\d+\.?\d*,\d+\.?\d*$/;
 
 const validator = new JustValidate(document.forms.linearForm, {
     validateBeforeSubmitting: true,
@@ -19,27 +19,47 @@ addField(point1, [{
     },
     {
         rule: 'customRegexp',
-        value: patternPoint,
+        value: PATTERN_POINT,
     },
 ]).addField(point2, [{
         rule: 'required',
     },
     {
         rule: 'customRegexp',
-        value: patternPoint,
+        value: PATTERN_POINT,
     },
 ]);
 
 function linearFormSubmit(event) {
+    let x = 0;
+    let z = 0;
     validator.eventListeners[0].func(event);
     const {
         isValid
     } = validator;
-    const [x1, z1] = document.forms.linearForm.point1.value.split(',');
-    const [x2, z2] = document.forms.linearForm.point2.value.split(',');
+    const {
+        point1: { value: point1 },
+        point2: { value: point2 },
+        xResult: { value: xResult },
+        zResult: { value: zResult },
+    } = linearForm;
+    
+    const [ x1, z1 ] = point1.split(',').map(e => Number(e));
+    const [ x2, z2 ] = point2.split(',').map(e => Number(e));
+    const k = ( z2 - z1 ) / ( x2 - x1 );
+    const b = z1 - k * x1;
 
-    alert(isValid);
-
+    if(!xResult && !zResult) return;
+    
+    if(!xResult) {
+      z = zResult;
+      x = ( b - z )/k;
+    } else {
+      x = xResult;
+      z = k * x + b;
+    }
+    linearForm.xResult.value = x;
+    linearForm.zResult.value = z;
 }
 
 linearForm.addEventListener('submit', linearFormSubmit);
